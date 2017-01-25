d8eae5d275439dad0384ccfa2693590dbce61ad4
azhar@oneorigin.us <azhar@oneorigin.us>
2017-01-25 17:53:10 +0530

    employees add

--- app.py
+++ app.py
@@ -49,6 +49,27 @@ def processRequest(req):
             "displayText": "Here's the list of Semesters you wanted : Fall, Spring, Summer",
             "source": "test-my-api-ai"
         }
+    if req.get("result").get("action") == "employee.search":
+        employee = req.get("result").get("parameters").get("employee")
+        
+        if employee == "azhar":
+            return {
+                "speech": "Azhar is mad.It's hard to explain abt him. I quit",
+                "displayText": "It's hard to explain abt him. I quit",
+                "source": "test-my-api-ai"
+            }
+        if employee == "arathi":
+            return {
+                "speech": "Arathi is mental and does not prefer any drink. Weired",
+                "displayText": "It's hard to explain abt him. I quit",
+                "source": "test-my-api-ai"
+            }
+        if employee == "mahesh":
+            return {
+                "speech": "mahesh is ios developer who's breaking his head at the moment. ha ha",
+                "displayText": "It's hard to explain abt him. I quit",
+                "source": "test-my-api-ai"
+            }
     return {
         "speech": "Sorry, I don't think you've asked the right thing!",
         "displayText": "Sorry, I don't think you've asked the right thing!",
