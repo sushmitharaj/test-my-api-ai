2c0d865e87ed4c32891cfc38e589542904613fde
azhar@oneorigin.us <azhar@oneorigin.us>
2017-01-25 20:22:24 +0530

    Data used as JSON

--- app.py
+++ app.py
@@ -37,43 +37,19 @@ def processRequest(req):
     if req.get("result").get("action") == "greet":
         return data["greet"][0]
     if req.get("result").get("action") == "course.search":
-        return {
-            "speech": "Here's the list of courses you wanted : EED 123, ABC 345, TTT 789",
-            "displayText": "Here's the list of courses you wanted : EED 123, ABC 345, TTT 789",
-            "source": "test-my-api-ai"
-        }
+        return data["course"]
     if req.get("result").get("action") == "semester.search":
-        return {
-            "speech": "Here's the list of Semesters you wanted : Fall, Spring, Summer",
-            "displayText": "Here's the list of Semesters you wanted : Fall, Spring, Summer",
-            "source": "test-my-api-ai"
-        }
+        return data["semester"]
     if req.get("result").get("action") == "employee.search":
         employee = req.get("result").get("parameters").get("employee")
         
         if employee == "azhar":
-            return {
-                "speech": "Azhar is mad.It's hard to explain abt him. I quit",
-                "displayText": "It's hard to explain abt him. I quit",
-                "source": "test-my-api-ai"
-            }
+            return data["users"]["azhar"]
         if employee == "arathi":
-            return {
-                "speech": "Arathi is mental and does not prefer any drink. Weired",
-                "displayText": "It's hard to explain abt him. I quit",
-                "source": "test-my-api-ai"
-            }
+            return data["users"]["arathi"]
         if employee == "mahesh":
-            return {
-                "speech": "mahesh is ios developer who's breaking his head at the moment. ha ha",
-                "displayText": "It's hard to explain abt him. I quit",
-                "source": "test-my-api-ai"
-            }
-    return {
-        "speech": "Sorry, I don't think you've asked the right thing!",
-        "displayText": "Sorry, I don't think you've asked the right thing!",
-        "source": "test-my-api-ai"
-    }
+            return data["users"]["mahesh"]
+    return data["sorry"]
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
